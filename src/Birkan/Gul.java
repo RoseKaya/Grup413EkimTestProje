a8fabfddef6ca0e8bfd1c3b41411b79768c71231
RoseKaya <gbuyruk@gmail.com>
2022-10-13 10:36:04 +0300

    Gul class ı eklendi

--- src/Birkan/Gul.java
+++ src/Birkan/Gul.java
@@ -1,4 +1,18 @@
 package Birkan;
 
 public class Gul {
+    public static void main(String[] args) {
+        System.out.println("merhaba dünya ");
+
+        for (int i = 0; i <5 ; i++ ) {
+            System.out.println("Merhaba1 "+i);
+        }
+
+        int sayac=0;
+        while (sayac<5 )
+        {
+            System.out.println("Merhaba GitHub1");
+            sayac++;
+        }
+    }
 }
